a7f1c54cfe5bec3154e50a5b8abd65f9ec54fb78
NiaMorgan29 <nemorgan95@gmail.com>
2024-08-11 22:45:11 -0400

    added additional weather data js updates

--- src/actions.js
+++ src/actions.js
@@ -2,9 +2,41 @@ function updateWeather(response) {
   let temperatureVariable = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityVariable = document.querySelector("#city");
+  let descriptionVariable = document.querySelector("#weather-description");
+  let humidityVariable = document.querySelector("#humidity");
+  let windSpeedVariable = document.querySelector("#wind-speed");
+  let timeVariable = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   cityVariable.innerHTML = response.data.city;
+  timeVariable.innerHTML = formatDate(date);
   temperatureVariable.innerHTML = Math.round(temperature);
+  descriptionVariable.innerHTML = response.data.condition.description;
+  humidityVariable.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedVariable.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
