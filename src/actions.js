3cf0ad3567e828c7bcd4dfc4421b5ba6b15e412c
NiaMorgan29 <nemorgan95@gmail.com>
2024-08-11 20:04:01 -0400

    update js weather functions

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,23 @@
+function updateWeather(response) {
+  let temperatureVariable = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityVariable = document.querySelector("#city");
+
+  cityVariable.innerHTML = response.data.city;
+  temperatureVariable.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "bc71af9e0b8fdta73d8ebob24352d13f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityVariable = document.querySelector("#city");
-  cityVariable.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
